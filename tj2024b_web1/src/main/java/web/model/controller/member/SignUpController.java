acbf3e200980acc6a2a9cb2898594e385ebea47a
ithodol <ddmdjesz1@gmail.com>
2025-01-27 15:11:58 +0900

    회원가입

---
+++ tj2024b_web1/src/main/java/web/model/controller/member/SignUpController.java
@@ -0,0 +1,33 @@
+package web.model.controller.member;
+
+import java.io.IOException;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import web.model.dao.MemberDao;
+import web.model.dto.MemberDto;
+
+@WebServlet("/member/signup")
+public class SignUpController extends HttpServlet {
+	// [회원가입]
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		System.out.println(" signup POST OK ");
+		// 1. HTTP 요청의 header body 자료(JSON)를 자바(DTO)로 받는다
+		ObjectMapper mapper = new ObjectMapper();
+		MemberDto memberDto = mapper.readValue(req.getReader(), MemberDto.class);
+		System.out.println("memberDto : " + memberDto);
+		// 2. 데이터 유효성검사
+		// 3. DAO에게 데이터 전달, 응답 받기
+		boolean result = MemberDao.getInstance().signup(memberDto);
+		// 4. 자료(DTO/자바타입) 타입을 JSON(JS) 타입으로 변환하기
+		// 5. HTTP 응답의 header body 로 application/json  으로 반환하기
+		resp.setContentType("application/json");
+		resp.getWriter().print(result);
+	}
+}
