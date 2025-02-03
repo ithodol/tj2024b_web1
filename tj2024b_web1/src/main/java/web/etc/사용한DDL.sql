3d2e2ab8aa39ba72ca9d2c4936488d5b1dceda54
ithodol <ddmdjesz1@gmail.com>
2025-02-03 14:38:46 +0900

    프로필 추가

--- tj2024b_web1/src/main/java/web/etc/사용한DDL.sql
+++ tj2024b_web1/src/main/java/web/etc/사용한DDL.sql
@@ -1,6 +1,9 @@
-drop database if exists jspweb;
-create database jspweb;
-use jspweb;
+# 프로젝트 : jspweb DML 과 샘플 INSERT
+# 1. DB 구성한다.
+drop database if exists jspweb;                
+create database jspweb;                           
+use jspweb;                                                  
+# 2. 테이블 생성  , PK 테이블 먼저 생성 한다. 적절하게 타입 과 제약조건을 고려한다.
 
 
 # [1] 회원테이블 생성 
@@ -11,6 +14,7 @@ create table member(
     mname varchar(20) not null ,
     mphone varchar(13) not null unique , 
     mdate datetime default now() , 
+    mimg varchar(255) default 'default.jpg',
     constraint primary key( mno )
 ); # table end 
 # [1] 회원테이블 샘플 레코드 삽입
@@ -19,5 +23,4 @@ insert into member ( mid , mpwd , mname , mphone ) values( 'asd123' , 'b123456' 
 insert into member ( mid , mpwd , mname , mphone ) values( 'zxc123' , 'c123456' , '신동엽' ,  '010-5555-5555' );
 insert into member ( mid , mpwd , mname , mphone ) values( 'vbn123' , 'd123456' , '서장훈' ,  '010-6666-6666' );
 insert into member ( mid , mpwd , mname , mphone ) values( 'rty123' , 'e123456' , '하하'   ,  '010-7777-7777' );
-
 select * from member;
