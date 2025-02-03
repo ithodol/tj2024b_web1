56813fe9548b392c6f77c9d5f01cc6dfb3413ac3
ithodol <ddmdjesz1@gmail.com>
2025-02-03 14:40:40 +0900

    index, header, footer

---
+++ tj2024b_web1/src/main/webapp/js/header.js
@@ -0,0 +1,56 @@
+console.log('header.js open');
+
+// [1] 로그인 정보 요청 함수
+const getLoginInfo = () => {
+	// 1.
+	const option = {
+		method : 'GET' // 생략 가능
+	}
+	
+	let loginmenu = document.querySelector('.loginmenu') // (1) 로그인 메뉴를 출력할 구역 가져오기
+	let html = ''; // (2) html 변수 선언
+
+	// 2.
+	fetch('/tj2024_web1/member/info', option)
+		.then( r => r.json())
+		.then(data => {
+			console.log(data); // 코드 변경 후 서버가 자동 재실행이 되면 세션 초기화
+			if(data == null){
+				console.log('비로그인');
+				// (3) 각 상태에 따라 로그인 메뉴 구성
+				html += `
+						<li class="nav-item"> <a class="nav-link" href="/tj2024b_web1/member/login.jsp">로그인</a> </li>
+						<li class="nav-item"> <a class="nav-link" href="/tj2024b_web1/member/signup.jsp">회원가입</a> </li>
+						 `
+			}else{
+				console.log('로그인');
+				// (3-1) 각 상태에 따라 로그인 메뉴 구성
+				html += `
+						<li class="nav-item">
+							<a class="nav-link" href="#"> <img class="header_profile" src=""/> ${data.mid} 님 </a>
+						</li>
+						<li class="nav-item"> <a class="nav-link" href="#" onclick="onLogOut()">로그아웃</a></li>
+						`
+			}
+			loginmenu.innerHTML = html;
+		})
+		.catch(e => {console.log(e);})
+}
+getLoginInfo(); // js가 열렸을 때 최초 1번 실행
+
+
+// [2] 로그아웃 요청 함수
+const onLogOut = () => {
+	const option = {
+		method : 'DELETE'
+	}
+	fetch('tj2024_web1/member/login', option)
+		.then(r => r.json())
+		.then(data => {
+			if(data == true){
+				alert('로그아웃 되었습니다');
+				location.href="/tj2024_web1/member/login.jsp"
+			}
+		})
+		.catch(e => {console.log(e);})
+}
