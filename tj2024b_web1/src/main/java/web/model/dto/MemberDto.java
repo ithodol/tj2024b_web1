6ff2edec206f4354be32c6e8730a0d5553a800d7
ithodol <ddmdjesz1@gmail.com>
2025-02-03 14:39:27 +0900

    프로필 추가

--- tj2024b_web1/src/main/java/web/model/dto/MemberDto.java
+++ tj2024b_web1/src/main/java/web/model/dto/MemberDto.java
@@ -10,13 +10,13 @@ import lombok.ToString;
 @NoArgsConstructor @AllArgsConstructor
 @Getter @Setter @ToString
 public class MemberDto {
-	
 	private int mno;		// 회원번호
 	private String mid;		// 아이디 
 	private String mpwd;	// 비밀번호
 	private String mname;	// 이름
 	private String mphone;	// 연락처 
 	private String mdate;	// 가입일 
+	private String mimg;	// 프로필
 
 	// @NoArgsConstructor
 	// public MemberDto()
